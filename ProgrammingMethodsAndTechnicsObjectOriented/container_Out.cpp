e29ebf31ffce5ea19ae8476f623eafcaf0e69eea
Andrew Andrew <bumer19981@gmail.com>
2020-04-13 01:55:10 +0700

    Оптимизирован код

--- ProgrammingMethodsAndTechnicsObjectOriented/container_Out.cpp
+++ ProgrammingMethodsAndTechnicsObjectOriented/container_Out.cpp
@@ -4,7 +4,7 @@ namespace simple_langtype {
     void container::Out(ofstream& ofst) {
         ofst << "Container contents " << list.size
             << " elements." << endl;
-        container::List::Node* tempHead = list.Head;
+        List::Node* tempHead = list.Head;
 
         int temp = list.size;
         while (temp != 0) {
